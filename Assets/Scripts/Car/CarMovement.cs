4ef03df94261ce4a9ccc011f167b1d8928d5a196
Jooglorystar <121430174+Jooglorystar@users.noreply.github.com>
2024-11-11 19:41:13 +0900

    Feat: Apply Car spawn system
    현재 Instantiate과 Destroy를 통해 구현, 추후 ObjectPool로 개선할 예정

--- Assets/Scripts/Car/CarMovement.cs
+++ Assets/Scripts/Car/CarMovement.cs
@@ -5,19 +5,22 @@ public class CarMovement : MonoBehaviour
     private Rigidbody _rigidbody;
     [SerializeField] private float speed;
 
-
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
 
     }
 
-    private void Start()
-    {
-    }
-
-    void LateUpdate()
+    private void LateUpdate()
     {
         _rigidbody.velocity = transform.forward.normalized * speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("CarRemover"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
---
+++ Assets/Scripts/Car/CarSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+    [SerializeField] private Transform carSpawnPoint;
+    [SerializeField] private GameObject carPrefab;
+
+    private Coroutine spawnCoroutine;
+    // TODO 오브젝트풀화 예정
+
+    private void Update()
+    {
+        SpawnCar();
+    }
+
+    private void SpawnCar()
+    {
+        if (spawnCoroutine != null) return;
+
+        spawnCoroutine = StartCoroutine(Spawn());
+    }
+
+    private IEnumerator Spawn()
+    {
+        GameObject createdCar = Instantiate(carPrefab, carSpawnPoint);
+        createdCar.transform.position = carSpawnPoint.position;
+        createdCar.transform.rotation = Quaternion.Euler(0f, 90f, 0f);
+        yield return new WaitForSeconds(3f);
+
+        spawnCoroutine = null;
+    }
+
+}
