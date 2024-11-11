73a260c6ead30e5790ba527dcde346f2cb0bef9e
Jooglorystar <121430174+Jooglorystar@users.noreply.github.com>
2024-11-11 10:29:42 +0900

    Feat: Apply camera tracking and player rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,7 +11,7 @@ public class PlayerController : MonoBehaviour
 
     private void Awake()
     {
-        _camera = Camera.main;   
+        _camera = Camera.main;
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -29,8 +29,15 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log(moveInput);
             transform.position += new Vector3(moveValue.x, 0, moveValue.y);
+            Rotate(moveValue);
             _camera.transform.position += new Vector3(moveValue.x, 0, moveValue.y);
             moveInput = Vector2.zero;
         }
+    }
+
+    private void Rotate(Vector3 moveValue)
+    {
+        transform.rotation = Quaternion.Euler(0f, moveValue.x * 90f, 0f);
+
     }
 }
